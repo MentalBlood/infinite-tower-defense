015039c7caa3488ae0238ac3a69c4079fdb36120
MentalBlood <necheporenko.stepan@yandex.ru>
2018-11-23 17:43:09 +0300

    Tower now shooting the nearest monster

--- game/shooting.cpp
+++ game/shooting.cpp
@@ -8,26 +8,38 @@ std::list<Shot*> shots;
 
 void tryToShoot(Tower *tower)
 {
-	sf::Vector2f vectorDistance;
+	float minDistanceSquare = tower->getRangeSquare();
+	Monster *nearestMonster = NULL;
+
 	for (std::list<Monster*>::iterator i = monsters.begin(); i != monsters.end(); i++)
 	{
-		vectorDistance = ((*i)->getPosition() - tower->getPosition()) / gameMap->getScale();
-		if ((vectorDistance.x * vectorDistance.x + vectorDistance.y * vectorDistance.y) <= tower->getRangeSquare())
-		{
-			if (tower->getShotType() == TYPE_A)
-				shots.push_back(new ShotTypeA(tower, (*i), sf::Color(30, 130, 30), sf::Color(130, 255, 130)));
-			else if (tower->getShotType() == TYPE_B)
-				shots.push_back(new ShotTypeB(tower, (*i), sf::Color::Blue, 16));
-			else if (tower->getShotType() == TYPE_C)
-				shots.push_back(new ShotTypeC(tower, (*i), sf::Color::Red));
-			else if (tower->getShotType() == TYPE_D)
-				shots.push_back(new ShotTypeD(tower, (*i)));
-			else if (tower->getShotType() == TYPE_E)
-				shots.push_back(new ShotTypeE(tower, (*i)));
+		float distanceSquareToThisMonster =
+			vectorLengthSquare(((*i)->getPosition() - tower->getPosition()) / gameMap->getScale());
 
-			new Timer<Tower*>(tower->getShotsDelay(), tryToShoot, tower);
-			return;
-		}
+		if (distanceSquareToThisMonster <= tower->getRangeSquare())
+			if (distanceSquareToThisMonster <= minDistanceSquare)
+			{
+				nearestMonster = (*i);
+				minDistanceSquare = distanceSquareToThisMonster;
+			}
 	}
-	new Timer<Tower*>(0, tryToShoot, tower);
+
+	if (!nearestMonster)
+	{		
+		new Timer<Tower*>(0, tryToShoot, tower);
+		return;
+	}
+
+	if (tower->getShotType() == TYPE_A)
+		shots.push_back(new ShotTypeA(tower, nearestMonster, sf::Color(30, 130, 30), sf::Color(130, 255, 130)));
+	else if (tower->getShotType() == TYPE_B)
+		shots.push_back(new ShotTypeB(tower, nearestMonster, sf::Color::Blue, 16));
+	else if (tower->getShotType() == TYPE_C)
+		shots.push_back(new ShotTypeC(tower, nearestMonster, sf::Color::Red));
+	else if (tower->getShotType() == TYPE_D)
+		shots.push_back(new ShotTypeD(tower, nearestMonster));
+	else if (tower->getShotType() == TYPE_E)
+		shots.push_back(new ShotTypeE(tower, nearestMonster));
+
+	new Timer<Tower*>(tower->getShotsDelay(), tryToShoot, tower);
 }
